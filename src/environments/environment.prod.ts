b6922ae74740b98b1dc926637ac0beb6ca3c5a6c
Jas Lieb <jason.liebault@outlook.com>
2021-12-05 13:31:43 +0100

    fix redirect #2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   user_id: '', // Please give your Client ID if you want to host the app in production your account 
               // Or leave a prompt make the job
-  redirect_url: 'https://jaslieb.github.io/randomSpot-angular/redirect'
+  redirect_url: 'https://jaslieb.github.io/randomSpot-angular/'
 };
