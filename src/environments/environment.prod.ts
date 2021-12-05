1c14e76d9e24a5a2ae7efd8cc45a87120c3a576e
Jas Lieb <jason.liebault@outlook.com>
2021-12-05 12:15:45 +0100

    V0.1 - Github Page

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  user_id: '' // Please give your Client ID if you want to host the app in production your account 
+  // Or leave a prompt make the job
+};
